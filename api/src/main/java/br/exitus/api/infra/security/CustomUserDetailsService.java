0f63e3af8edd36308766d2b92153a9e14524d246
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-14 21:47:10 -0300

    feat: creates custom user details service

---
+++ api/src/main/java/br/exitus/api/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package br.exitus.api.infra.security;
+
+import br.exitus.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
+}
