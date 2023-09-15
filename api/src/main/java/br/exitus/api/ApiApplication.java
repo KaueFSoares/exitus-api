f751155ed7ec734deef8e36bdeae99ec81140c69
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-15 20:57:15 -0300

    feat: enables jpa repositories

--- api/src/main/java/br/exitus/api/ApiApplication.java
+++ api/src/main/java/br/exitus/api/ApiApplication.java
@@ -2,8 +2,10 @@ package br.exitus.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories({"br.exitus.api.repository"})
 public class ApiApplication {
 
     public static void main(String[] args) {
