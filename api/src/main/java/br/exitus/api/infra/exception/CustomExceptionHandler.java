f252f68475df86c6a8489a1c0e243f10e7176218
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-15 20:03:45 -0300

    feat: updates exception handler

--- api/src/main/java/br/exitus/api/infra/exception/CustomExceptionHandler.java
+++ api/src/main/java/br/exitus/api/infra/exception/CustomExceptionHandler.java
@@ -2,11 +2,13 @@ package br.exitus.api.infra.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @Component
@@ -30,8 +32,22 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<ValidationErrorResponseDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        var errors = e.getFieldErrors();
-        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorResponseDTO::new).toList());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getFieldErrors().stream().map(ValidationErrorResponseDTO::new).toList());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 
 }
