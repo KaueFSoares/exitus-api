9ef7ad95f491876e0c73184c3a6ed8f549f4c0ba
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-15 20:59:08 -0300

    refactor: signs transactional methods

--- api/src/main/java/br/exitus/api/domain/role/RoleSeeder.java
+++ api/src/main/java/br/exitus/api/domain/role/RoleSeeder.java
@@ -2,6 +2,7 @@ package br.exitus.api.domain.role;
 
 import br.exitus.api.repository.RoleRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ public class RoleSeeder {
         this.roleRepository = roleRepository;
     }
 
+    @Transactional
     @PostConstruct
     public void seed() {
         if (roleRepository.count() == 0) {
