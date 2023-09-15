89a032c98865c397fba951a804f69e75a571ccc6
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-14 21:46:24 -0300

    refactor: updates SecutiryConfig for providing authentication manager and password encoder

--- api/src/main/java/br/exitus/api/infra/security/SecurityConfig.java
+++ api/src/main/java/br/exitus/api/infra/security/SecurityConfig.java
@@ -1,11 +1,17 @@
 package br.exitus.api.infra.security;
 
+import br.exitus.api.constant.message.AuthMessages;
+import br.exitus.api.infra.exception.AuthException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,9 +24,25 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests( authorize -> authorize
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/signup").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        try {
+            return authenticationConfiguration.getAuthenticationManager();
+        } catch (Exception e) {
+            throw new AuthException(AuthMessages.ERROR_AUTHENTICATION_MANAGER);
+        }
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
