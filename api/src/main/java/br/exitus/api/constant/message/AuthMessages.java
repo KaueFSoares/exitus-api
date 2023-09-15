abd79eaae2424397c46e11738a57b145f184bba6
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-14 21:45:11 -0300

    feat: creates static messages classes

---
+++ api/src/main/java/br/exitus/api/constant/message/AuthMessages.java
@@ -0,0 +1,5 @@
+package br.exitus.api.constant.message;
+
+public class AuthMessages {
+    public static String ERROR_AUTHENTICATION_MANAGER = "Error on getting authentication manager.";
+}
---
+++ api/src/main/java/br/exitus/api/constant/message/UserValidationMessages.java
@@ -0,0 +1,5 @@
+package br.exitus.api.constant.message;
+
+public class UserValidationMessages {
+    public static final String EMAIL_DUPLICATED = "Email already in use.";
+}
