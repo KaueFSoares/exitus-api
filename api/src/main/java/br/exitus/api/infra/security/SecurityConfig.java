51f8895cf26b46f35f6fa67801382ae706d43eaf
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-15 22:23:25 -0300

    feat: opens the swagger ui end point

--- api/src/main/java/br/exitus/api/infra/security/SecurityConfig.java
+++ api/src/main/java/br/exitus/api/infra/security/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(RouteVAR.FULL_LOGIN).permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
