9a51bbdafef68f49a9a126438a7e9a8903996f2a
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-19 20:10:12 -0300

    feat: creates the auth service with is guarded and is employee methods

---
+++ api/src/main/java/br/exitus/api/service/AuthService.java
@@ -0,0 +1,34 @@
+package br.exitus.api.service;
+
+import br.exitus.api.domain.role.RoleEnum;
+import br.exitus.api.domain.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final RoleService roleService;
+
+    @Autowired
+    public AuthService(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    public Boolean isGuarded(User user) {
+        var roles = user.getRoles();
+
+        var guardedRole = roleService.getRoleByEnum(RoleEnum.GUARDED);
+
+        return roles.contains(guardedRole);
+    }
+
+    public Boolean isEmployee(User user) {
+        var roles = user.getRoles();
+
+        var employeeRole = roleService.getRoleByEnum(RoleEnum.EMPLOYEE);
+
+        return roles.contains(employeeRole);
+    }
+
+}
