fe60a29a1932b6df58a1b3fee7e464428c6526bf
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-19 20:09:08 -0300

    refactor: makes front end url a static var

--- api/src/main/java/br/exitus/api/infra/security/CorsConfig.java
+++ api/src/main/java/br/exitus/api/infra/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package br.exitus.api.infra.security;
 
+import br.exitus.api.constant.variable.RouteVAR;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                        .allowedOrigins("http://localhost:5173");
+                        .allowedOrigins(RouteVAR.FRONTEND);
             }
         };
     }
